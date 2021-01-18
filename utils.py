45584ccbc4f7c4865eb561a1a7e9d7aa13410038
Jorjiang23@gmail.com <JIjiang23>
2021-01-18 20:36:50 +0100

    fix bug

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ IMG = Image.Image
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Some constants
-rgb_weights = torch.FloatTensor([65.481, 128.553, 24.966]).to("cpu")
+rgb_weights = torch.FloatTensor([65.481, 128.553, 24.966]).to(device)
 imagenet_mean = torch.FloatTensor([0.485, 0.456, 0.406]).unsqueeze(1).unsqueeze(2)
 imagenet_std = torch.FloatTensor([0.229, 0.224, 0.225]).unsqueeze(1).unsqueeze(2)
 imagenet_mean_cuda = torch.FloatTensor([0.485, 0.456, 0.406]).to(device).unsqueeze(0).unsqueeze(2).unsqueeze(3)
